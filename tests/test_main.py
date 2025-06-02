d6a83c66b3ffba0a1cf4ab1e0c78d692ff3643e6
Filipe Galo <fcostagalo@gmail.com>
2025-06-02 01:50:09 +0100

    test: fix failing tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_main_route():
 	response = client.get('/main')
 	assert response.status_code == 200
-	assert response.json() == {'message': 'Hey, good job!'}
+	assert response.json() == {'message': 'Hey, good job! Good job!'}
 	assert response.headers['content-type'] == 'application/json'
 
 
